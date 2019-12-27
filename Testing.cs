c1d63da6f6b97b8b614a2135440abc5b60b81db7
Qihong <qihongjiang123@gmail.com>
2019-12-27 18:36:39 -0500

    Not Reponding Fix

--- Testing.cs
+++ Testing.cs
@@ -10,6 +10,9 @@ public class Testing : MonoBehaviour
     private Grid grid;
     private int h, v;
     private int ch, cv;
+    private float mouseX, mouseY;
+
+    private Vector3 vec;
 
     private void Start()
     {
@@ -18,37 +21,48 @@ public class Testing : MonoBehaviour
 
     private void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             vec.z = 0f;
-
             GetXY(vec, out h, out v);
-
-            grid.SetValue(vec, 1);
 		}
         else if (Input.GetMouseButtonUp(0))
         {
-            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             vec.z = 0f;
 
             grid.SetValue(vec, 3);
         }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0)) {
+            vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             vec.z = 0f;
+            mouseX = Input.GetAxis("Mouse X");
+            mouseY = Input.GetAxis("Mouse Y");
 
-            GetXY(vec, out ch, out cv);
+            if (mouseX > 0 || mouseY > 0) { GetXY(vec, out ch, out cv); }
 
-            if (isLinear(h, v, ch, cv)) {
-                grid.SetValue(vec, 2);
-                MonoBehaviour.print("Linear");
+            if (IsHorizontal(h, v, ch, cv) && mouseY <= 0.05 && mouseX > 0){
+                grid.SetValue(vec, 5);
+                MonoBehaviour.print("Mouse X: " + mouseX);
+                MonoBehaviour.print("Mouse Y: " + mouseY);
+                MonoBehaviour.print("Horizontal: ");
             }
+
+            if (IsDiagonal(h, v, ch, cv) && mouseY > 0.1 && mouseX > 0.1){ grid.SetValue(vec, 2);}
         }
     }
 
-    private bool isLinear(int x1, int y1, int x2, int y2) {
-        if (Math.Abs(x1 - x2) == Math.Abs(y1 - y2))
+    private bool IsHorizontal(int x1, int y1, int x2, int y2)
+    {
+        if ((Math.Abs(y1 - y2)==0)&& (Math.Abs(x1 - x2) == 1))
         {
+            return true;
+        }
+        return false;
+    }
+
+    private bool IsDiagonal(int x1, int y1, int x2, int y2) {
+        if (Math.Abs(x1 - x2) == Math.Abs(y1 - y2)){
             return true;
         }
         return false;
