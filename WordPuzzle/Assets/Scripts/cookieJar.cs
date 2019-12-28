c46adeb1984063fa561f1a86861fd2c2af01d72f
Maggie Zhou <maggie.zhou@stonybrook.edu>
2019-12-28 11:40:16 -0500

    word puzzle skeleton

---
+++ WordPuzzle/Assets/Scripts/cookieJar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class cookieJar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown() {
+        cookieController.curSpeltWord += GetComponent<TextMeshPro>().text;
+        Debug.Log(cookieController.curSpeltWord);
+        GetComponent<TextMeshPro>().enabled = !GetComponent<TextMeshPro>().enabled;
+    }
+}
